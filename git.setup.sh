6bacf62e3091dfe638b5d3e3682e8d14fb105916
Louie Kwan <louie.kwan@windriver.com>
2018-05-14 10:22:34 -0400

    git setup

---
+++ git.setup.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+git config --global user.email "louie.kwan@windriver.com"
+git config --global user.name "Louie Kwan"
+git config --global gitreview.username "lkwan"
+git config --global core.editor "vim"
+
+#
+git clone https://git.openstack.org/openstack-dev/sandbox.git ~/sandbox
+#
+git clone https://git.openstack.org/openstack-dev/devstack -b stable/queens ~/devstack
+#
+cp `pwd`/local.conf ~/devstack
